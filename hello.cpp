a27d37cf73fafd1e7f804d03e0f478b691eb56ba
Rumpelstilzchen <Rumpelstilzchen2018@yandex.ru>
2021-03-07 00:29:12 +0300

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+﻿#include "hello.h"
+using namespace std;
+
+string Hello::hello()
+{
+	return "Hello, World!";
+}
---
+++ hello.h
@@ -0,0 +1,15 @@
+#ifndef HELLO
+#define HELLO
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Hello
+{
+private:
+public:
+	static string hello();
+};
+
+#endif //HELLO

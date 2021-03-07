c5186ea4b94c08ea8446274a69aaf973cdd772d4
Rumpelstilzchen <Rumpelstilzchen2018@yandex.ru>
2021-03-07 18:54:14 +0300

    2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	cout << Hello().hello();
-	cout << 3 << " + " << 2 << " = " << 3 + 2 << endl;
+	cout << 3 << " * " << 2 << " = " << 3 * 2 << endl;
 	cout << "I also ♥ math\n";
 	return 0;
 }
